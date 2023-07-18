433f06c2a5868b7763a54981040a218ee68d07d4
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-07-19 00:58:07 +0300

    first commit

---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
